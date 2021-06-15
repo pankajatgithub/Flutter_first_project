d34f73b2b359e878eec9cd3639e8a55ab839ff67
pankajatgithub <pankaj200635@gmail.com>
2021-06-16 04:23:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() =>  runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("My First App"),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("Hello Pankaj"),
+    ),
+    floatingActionButton: FloatingActionButton(
+     child: Text("click"),
+
+    ),
+  )
+));
+
+
+
